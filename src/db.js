25930748e98c5488c967eae6d3220be45b408ffd
jonathan <jonathan.araya.m@gmail.com>
2020-02-04 04:28:32 -0300

    kk

--- src/db.js
+++ src/db.js
@@ -1,12 +1,29 @@
 import mongoose from 'mongoose';
 export const connect = async () => {
+  mongoose.Promise = global.Promise;
+ let mongoUserCredentials;
+ console.log('per', process.env.MONGO_USER);
+  if (process.env.MONGO_USER && process.env.MONGO_PASSWORD) {
+    mongoUserCredentials = `${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}`;
+  }
+
+  // const MONGO_URL = process.env.MONGO_URL || 'localhost:27017';
+  // const MONGO_DB_NAME = process.env.MONGO_DB_NAME || 'graphql-test';
+  
+  const MONGO_DB_NAME='graphql-test';
+  const MONGO_URL='cluster0-2mk0o.mongodb.net';
+  const MONGO_USER='qwerty0';
+  const MONGO_PASSWORD='2JFgDggA9X6OuYCK';
+
+  const MONGO_CONNECTION_STRING = `mongodb+srv://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_URL}/${MONGO_DB_NAME}?retryWrites=true&w=majority`;
+
   try {
-    await mongoose.connect('mongodb+srv://qwerty0:2JFgDggA9X6OuYCK@cluster0-2mk0o.mongodb.net/graphql-test?retryWrites=true&w=majority', {
+    await mongoose.connect(MONGO_CONNECTION_STRING, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
     console.log('db is connected');
-  } catch {
-    console.log('Error db');
+  } catch (e){
+    console.log('Error db', e);
   }
 }
